071cfd1b72e0c2c1623e8b96efb2fadc031b0dc5
Zhoudunming Tu <tuzhoudunming@gmail.com>
2014-10-03 18:48:37 +0200

    Add the tracks from iter 6 and 7

--- HiTracking/python/MergeRegitTrackCollectionsHI_cff.py
+++ HiTracking/python/MergeRegitTrackCollectionsHI_cff.py
@@ -6,16 +6,20 @@ hiRegitTracks = RecoTracker.FinalTrackSelectors.trackListMerger_cfi.trackListMer
                       cms.InputTag('hiRegitLowPtTripletStepTracks'),
                       cms.InputTag('hiRegitPixelPairStepTracks'),
                       cms.InputTag('hiRegitDetachedTripletStepTracks'),
-                      cms.InputTag('hiRegitMixedTripletStepTracks')),
-    hasSelector=cms.vint32(1,1,1,1,1),
+                      cms.InputTag('hiRegitMixedTripletStepTracks'),
+		      cms.InputTag('hiRegitPixelLessStepTracks'),
+		      cms.InputTag('hiRegitTobTecStepTracks')),
+    hasSelector=cms.vint32(1,1,1,1,1,1,1),
     selectedTrackQuals = cms.VInputTag(
     cms.InputTag("hiRegitInitialStepSelector","hiRegitInitialStep"),
     cms.InputTag("hiRegitLowPtTripletStepSelector","hiRegitLowPtTripletStep"),
     cms.InputTag("hiRegitPixelPairStepSelector","hiRegitPixelPairStep"),
     cms.InputTag("hiRegitDetachedTripletStepSelector","hiRegitDetachedTripletStep"),
     cms.InputTag("hiRegitMixedTripletStepSelector","hiRegitMixedTripletStep"),
+    cms.InputTag("hiRegitPixelLessStepSelector","hiRegitPixelLessStep"),
+    cms.InputTag("hiRegitTobTecStepSelector","hiRegitTobTecStep"),
     ),                    
-    setsToMerge = cms.VPSet( cms.PSet( tLists=cms.vint32(0,1,2,3,4,5), pQual=cms.bool(True)),  # should this be False?
+    setsToMerge = cms.VPSet( cms.PSet( tLists=cms.vint32(0,1,2,3,4,5,6,7), pQual=cms.bool(True)),  # should this be False?
                              ),
     copyExtras = True,
     makeReKeyedSeeds = cms.untracked.bool(False)
